69b183e300b791a19b832638ad5bad2139e42773
Jettan17 <jethrotan2006@gmail.com>
2025-07-30 13:54:03 +0800

    Adds more text to app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    return "Hello, World!faunajnfanfosafnioasnfioafnaj fij"
 
 if __name__ == "__main__":
     app.run(port=os.environ.get('PORT', 5000), host="0.0.0.0", debug=True)
